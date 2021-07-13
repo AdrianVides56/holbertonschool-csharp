1dcfb4df782c075acdeb50085111bd5c0d4a3eb0
AdrianVides56 <adrianvj18@gmail.com>
2021-07-12 19:35:47 -0500

    Added task 6

---
+++ 0x00-csharp-hello_world/6-print_string/6-print_string.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		string str = "Holberton School";
+		Console.Write("{0}{0}{0}\n{1}\n", str, str.Substring(0,9));
+    }
+}
