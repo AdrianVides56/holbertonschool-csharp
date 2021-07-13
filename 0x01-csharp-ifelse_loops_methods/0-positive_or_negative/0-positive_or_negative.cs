57595bd31a3571d9819b00b8f884f157226cd9fa
AdrianVides56 <adrianvj18@gmail.com>
2021-07-13 14:19:39 -0500

    Added task 0

---
+++ 0x01-csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rndm = new Random();
+        int number = rndm.Next(-10, 10);
+        if (number > 0)
+        {
+            Console.Write(number + " is positive\n");
+        }
+        else if (number < 0)
+        {
+            Console.Write(number + " is negative\n");
+        }
+        else
+            Console.Write(number + " is zero\n");
+    }
+}
