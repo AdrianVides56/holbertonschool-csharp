7d5a00f51a37709d81445da038e495f279f4cf0e
AdrianVides56 <adrianvj18@gmail.com>
2021-07-15 18:18:00 -0500

    Added task 9

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+
+class LList
+{
+	public static int Pop(LinkedList<int> myLList)
+	{
+		try
+		{
+			int data = myLList.First.Value;
+			myLList.RemoveFirst();
+			return data;
+		}
+		catch (InvalidOperationException)
+		{
+			return 0;
+		}
+	}
+}
