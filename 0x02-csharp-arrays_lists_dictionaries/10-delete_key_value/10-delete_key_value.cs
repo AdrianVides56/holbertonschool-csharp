18029d6239cd83150b8cedf4ebea8ad9d7b0956f
AdrianVides56 <adrianvj18@gmail.com>
2021-07-14 22:01:36 -0500

    Removed unnecesary comment

--- 0x02-csharp-arrays_lists_dictionaries/10-delete_key_value/10-delete_key_value.cs
+++ 0x02-csharp-arrays_lists_dictionaries/10-delete_key_value/10-delete_key_value.cs
@@ -5,12 +5,7 @@ class Dictionary
 {
     public static Dictionary<string, string> DeleteKeyValue(Dictionary<string, string> myDict, string key)
     {
-        /* if (myDict[key])
-        {
-
-        } */
         myDict.Remove(key);
-
         return myDict;
     }
 }
