f8580d0d54417517c97dc3d419634436d7062ea7
AdrianVides56 <adrianvj18@gmail.com>
2021-07-14 20:16:15 -0500

    Added print when list is empty

--- 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
+++ 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -6,7 +6,10 @@ class List
     public static int MaxInteger(List<int> myList)
     {
         if (myList.Capacity == 0 || myList == null)
+        {
+            System.Console.WriteLine("List is empty");
             return -1;
+        }
         myList.Sort();
         return myList[myList.Count - 1];
     }
