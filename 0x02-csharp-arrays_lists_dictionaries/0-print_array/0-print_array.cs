7962398708790495724f399a0c209ffc9b2d9220
AdrianVides56 <adrianvj18@gmail.com>
2021-07-14 19:09:09 -0500

    Added task 0

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,33 @@
+using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return (null);
+        }
+
+        int[] myArray = new int[size];
+        if (size == 0)
+        {
+            Console.WriteLine("");
+        }
+        else
+        {
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                myArray[i] = i;
+                if (i != myArray.Length - 1)
+                    Console.Write(myArray[i]+ " ");
+                else
+                    Console.Write(myArray[i]);
+            }
+            Console.WriteLine("");
+        }
+        
+        return myArray;
+    }
+}
