2e98e8f326f10ffe58dd0cc5ddb8130a8a6218f4
AdrianVides56 <adrianvj18@gmail.com>
2021-07-17 18:13:13 -0500

    Added conditional if value is 0

--- 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -10,10 +10,20 @@ class Dictionary
 
 		if (myList == null || myList.Count == 0)
 			return "None";
+		else if (myList.Count == 1)
+		{
+			foreach (var item in myList)
+			{
+				if (item.Value == 0)
+				{
+					return "None";
+				}
+			}
+		}
 
 		foreach (KeyValuePair<string, int> pair in myList)
 		{
-			if (pair.Value > best)
+			if (pair.Value > best && pair.Value != 0)
 			{
 				key = pair.Key;
 				best = pair.Value;
