5c77e27d13816ea0ea1d7e2dedfbc1ceb23f226e
AdrianVides56 <adrianvj18@gmail.com>
2021-07-17 17:49:16 -0500

    Added task 13

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+	public static string BestScore(Dictionary<string, int> myList)
+	{
+		string key = null;
+		int best = 0;
+
+		foreach (KeyValuePair<string, int> pair in myList)
+		{
+			if (pair.Value > best)
+			{
+				key = pair.Key;
+				best = pair.Value;
+			}
+		}
+		return key;
+	}
+}
