08ec178905359b467a010349fb201978aa4195ca
AdrianVides56 <adrianvj18@gmail.com>
2021-07-27 19:15:23 -0500

    Added task 1

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,12 @@
+namespace Enemies
+{
+	///<summary>Defines a Zombie.</summary>
+	public class Zombie
+	{
+		///<summary>Zombie health.</summary>
+		public int health;
+
+		///<summary>Initialize a new instance of <see cref="Zombie"/>.</summary>
+		public Zombie() => health = 0;
+	}
+}
