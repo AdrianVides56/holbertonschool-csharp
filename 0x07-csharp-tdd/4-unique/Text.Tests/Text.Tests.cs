bc0f898924ffbf972ffd382162c6dafc28420d7f
AdrianVides56 <adrianvj18@gmail.com>
2021-07-29 15:38:31 -0500

    Added task 4

---
+++ 0x07-csharp-tdd/4-unique/Text.Tests/Text.Tests.cs
@@ -0,0 +1,43 @@
+using NUnit.Framework;
+
+namespace Text.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void FirstUniqueChar([Values("hello", "true")] string s)
+        {
+            int result = Str.UniqueChar(s);
+            Assert.AreEqual(result, 0);
+        }
+
+        [Test]
+        public void NoUniqueChar([Values("tddt", "hhoommee")] string s)
+        {
+            int result = Str.UniqueChar(s);
+            Assert.AreEqual(result, -1);
+        }
+
+        [Test]
+        public void LastIsUniqueChar([Values("soosy")] string s)
+        {
+            int result = Str.UniqueChar(s);
+            Assert.AreEqual(result, 4);
+        }
+
+        [Test]
+        public void EmptyString([Values("")] string s)
+        {
+            int result = Str.UniqueChar(s);
+            Assert.AreEqual(result, -1);
+        }
+
+        [Test]
+        public void OneLengthString([Values(" ", "\"", "t")] string s)
+        {
+            int result = Str.UniqueChar(s);
+            Assert.AreEqual(result, 0);
+        }
+    }
+}
