dfc59927dba62fc60524bd0a7c5d5cfff0b26368
AdrianVides56 <adrianvj18@gmail.com>
2021-07-29 11:31:08 -0500

    Added more test cases

--- 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
@@ -1,6 +1,5 @@
 using NUnit.Framework;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace MyMath.Tests
 {
@@ -12,7 +11,7 @@ namespace MyMath.Tests
         {
             List<int> nums = new List<int>{ 1, 2, 3, 4, 5, 6 };
             int result = Operations.Max(nums);
-            Assert.AreEqual(result, nums.Max());
+            Assert.AreEqual(result, 6);
         }
 
         [Test]
@@ -20,7 +19,7 @@ namespace MyMath.Tests
         {
             List<int> nums = new List<int>{ 2, 1, 6, 4, 7, 1, 98, 23, 93 };
             int result = Operations.Max(nums);
-            Assert.AreEqual(result, nums.Max());
+            Assert.AreEqual(result, 98);
         }
 
         [Test]
@@ -29,6 +28,30 @@ namespace MyMath.Tests
             List<int> nums = new List<int>();
             int result = Operations.Max(nums);
             Assert.AreEqual(result, 0);
+        }
+
+        [Test]
+        public void LengthOneList()
+        {
+            List<int> nums = new List<int>{ 90 };
+            int result = Operations.Max(nums);
+            Assert.AreEqual(result, 90);
+        }
+
+        [Test]
+        public void MultpleMaximuns()
+        {
+            List<int> nums = new List<int>{ 23, 23, 23, 2, 22, 10 };
+            int result = Operations.Max(nums);
+            Assert.AreEqual(result, 23);
+        }
+
+        [Test]
+        public void NegativeIntegers()
+        {
+            List<int> nums = new List<int>{ -50, -48, -100, -999, -5};
+            int result = Operations.Max(nums);
+            Assert.AreEqual(result, -5);
         }
     }
 }
