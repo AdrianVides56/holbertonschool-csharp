ae5bb6b995be03a29bdad14afa4ad357be5ed25f
AdrianVides56 <adrianvj18@gmail.com>
2021-07-29 13:01:39 -0500

    Added task 3

---
+++ 0x07-csharp-tdd/3-palindrome/Text.Tests/Text.Tests.cs
@@ -0,0 +1,50 @@
+using NUnit.Framework;
+
+namespace Text.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void RegularString([Values("level", "racecar")] string s)
+        {
+            bool result = Str.IsPalindrome(s);
+            Assert.AreEqual(result, true);
+        }
+
+        [Test]
+        public void CaseSensitiveString([Values("Level", "racecaR")] string s)
+        {
+            bool result = Str.IsPalindrome(s);
+            Assert.AreEqual(result, true);
+        }
+
+        [Test]
+        public void StringWithPunctuation([Values("A man, a plan, a canal: Panama.")] string s)
+        {
+            bool result = Str.IsPalindrome(s);
+            Assert.AreEqual(result, true);
+        }
+
+        [Test]
+        public void EmptyString([Values("")] string s)
+        {
+            bool result = Str.IsPalindrome(s);
+            Assert.AreEqual(result, true);
+        }
+
+        [Test]
+        public void NotPalindrome([Values("This", "is", "not", "palindrome")] string s)
+        {
+            bool result = Str.IsPalindrome(s);
+            Assert.AreEqual(result, false);
+        }
+
+        [Test]
+        public void StringWithQuotes([Values("\"\"")] string s)
+        {
+            bool result = Str.IsPalindrome(s);
+            Assert.AreEqual(result, true);
+        }
+    }
+}
---
+++ 0x07-csharp-tdd/3-palindrome/Text/Text.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Text
+{
+    public class Str
+    {
+        public static bool IsPalindrome(string s)
+        {
+            s = s.ToLower();
+
+            for (int i = 0, j = s.Length - 1; i < s.Length; i++)
+            {
+                if (!Char.IsLetterOrDigit(s[i]) || !Char.IsLetterOrDigit(s[j]))
+                    continue;
+                else if (s[i] != s[j])
+                    return false;
+                j--;
+            }
+            return true;
+        }
+    }
+}
