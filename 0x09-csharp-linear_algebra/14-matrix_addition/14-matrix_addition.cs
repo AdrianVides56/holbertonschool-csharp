baed50a11e160fe4c5f37d9494fe2228cfb72c96
AdrianVides56 <adrianvj18@gmail.com>
2021-08-21 15:22:09 -0500

    Added task 14

---
+++ 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -0,0 +1,20 @@
+///<summary>Initialize class.</summary>
+class VectorMath
+{
+	///<summary>Adds two matrices.</summary>
+	public static double[,] Add(double[,] matrix1, double[,] matrix2)
+	{
+		if (matrix1.Length != matrix2.Length || matrix1.GetLength(0) < 2 || matrix1.GetLength(0) > 3 ||
+			matrix2.GetLength(0) < 2 || matrix2.GetLength(0) > 3)
+			return new double[,] { { -1 } };
+		double[,] result = new double[matrix1.GetLength(0), matrix2.GetLength(1)];
+
+		for (int i = 0; i < matrix1.GetLength(0); i++)
+		{
+			for (int j = 0; j < matrix1.GetLength(1); j++)
+				result[i, j] = matrix1[i, j] + matrix2[i, j];
+		}
+
+		return result;
+	}
+}
