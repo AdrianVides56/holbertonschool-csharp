ac3c0a639d4a04060dcbf49355a3cac083ede91e
AdrianVides56 <adrianvj18@gmail.com>
2021-08-21 16:56:18 -0500

    Changed conditional statement

--- 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
+++ 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -4,8 +4,7 @@ class MatrixMath
 	///<summary>Multyplies a matrix by a number.</summary>
 	public static double[,] MultiplyScalar(double[,] matrix, double scalar)
 	{
-		if (matrix.GetLength(0) < 2 || matrix.GetLength(0) > 3 ||
-			matrix.GetUpperBound(0) < 2 || matrix.GetUpperBound(0) > 3)
+		if (Check(matrix))
 			return new double[,] { { -1 } };
 
 		double[,] result = new double[matrix.GetLength(0), matrix.GetLength(1)];
@@ -17,5 +16,14 @@ class MatrixMath
 		}
 
 		return result;
+	}
+
+	public static bool Check(double[,] matrix)
+	{
+		if (matrix.GetLength(0) < 2 || matrix.GetLength(0) > 3)
+			return true;
+		if (matrix.GetLength(1) < 2 || matrix.GetLength(1) > 3)
+			return true;
+		return false;
 	}
 }
