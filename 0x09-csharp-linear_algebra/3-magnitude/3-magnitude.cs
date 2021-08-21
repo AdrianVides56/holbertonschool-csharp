b6f751971bcb0ebecff9c9448a9db3551e9c0f6a
AdrianVides56 <adrianvj18@gmail.com>
2021-08-21 13:30:22 -0500

    Added namespace

--- 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -1,18 +1,21 @@
-using System;
+﻿using System;
 
-///<summary>Initialize class.</summary>
-class VectorMath
+namespace _3_magnitude
 {
-	///<summary>Calculates the length of a given vector.</summary>
-	public static double Magnitude(double[] vector)
-	{
-		double sum = 0;
-		if (vector.Length < 2)
-			return -1;
-		foreach (var i in vector)
-			sum += Math.Pow(i, 2);
-		sum = Math.Sqrt(sum);
+    ///<summary>Initialize class.</summary>
+    class VectorMath
+    {
+        ///<summary>Calculates the length of a given vector.</summary>
+        public static double Magnitude(double[] vector)
+        {
+            double sum = 0;
+            if (vector.Length < 2)
+                return -1;
+            foreach (var i in vector)
+                sum += Math.Pow(i, 2);
+            sum = Math.Sqrt(sum);
 
-		return Math.Round(sum, 2);
-	}
+            return Math.Round(sum, 2);
+        }
+    }
 }
