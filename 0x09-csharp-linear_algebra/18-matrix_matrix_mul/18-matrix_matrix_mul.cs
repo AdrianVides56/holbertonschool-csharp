1ebbcb9d48274338bf97e1a9d7c021e00bb0a507
AdrianVides56 <adrianvj18@gmail.com>
2021-08-23 11:58:37 -0500

    Added task 18

---
+++ 0x09-csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -0,0 +1,28 @@
+///<summary>Initializes class.</summary>
+class MatrixMath
+{
+	///<summary>Multiplies two matrices.</summary>
+	public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
+	{
+		double rowsA = matrix1.GetLength(0), colsA = matrix1.GetLength(1);
+		double rowsB = matrix2.GetLength(0), colsB = matrix2.GetLength(1);
+		double[,] res = new double[(int)rowsA, (int)colsB];
+		double aux = 0;
+
+		if (colsA != rowsB) // checks if the matrices can be multiplied
+			return new double[,] {{ -1 }};
+
+		for (int row = 0; row < rowsA; row++)
+		{
+			for (int col= 0; col < colsB; col++)
+			{
+				aux = 0;
+				for (int idx = 0; idx < rowsB; idx++)
+					aux += matrix1[row, idx] * matrix2[idx, col];
+				res[row, col] = aux;
+			}
+		}
+
+		return res;
+	}
+}
