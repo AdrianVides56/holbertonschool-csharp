2226b7ca32e5679d2d84ee14937b7bbc256df653
AdrianVides56 <adrianvj18@gmail.com>
2021-08-24 14:28:26 -0500

    Added task 23

---
+++ 0x09-csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
@@ -0,0 +1,19 @@
+///<summary>Initialize class.</summary>
+class MatrixMath
+{
+	public static double[,] Transpose(double[,] matrix)
+	{
+		if (matrix.Length == 0)
+			return matrix;
+
+		double[,] trans = new double[matrix.GetLength(1), matrix.GetLength(0)];
+
+		for (int i = 0; i < matrix.GetLength(0); i++)
+		{
+			for (int j = 0; j < matrix.GetLength(1); j++)
+				trans[j, i] = matrix[i, j];
+		}
+
+		return trans;
+	}
+}
