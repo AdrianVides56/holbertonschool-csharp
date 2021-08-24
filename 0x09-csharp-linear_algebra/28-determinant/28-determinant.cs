bb6850e628a97af166c9c7ec6eb92e865c6ea2b6
AdrianVides56 <adrianvj18@gmail.com>
2021-08-24 18:52:02 -0500

    If it's not a square matrix return -1

--- 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
+++ 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -5,7 +5,8 @@ class MatrixMath
 	public static double Determinant(double[,] matrix)
 	{
 		if (matrix.GetLength(0) < 2 || matrix.GetLength(0) > 3 ||
-			matrix.GetLength(1) < 2 || matrix.GetLength(1) > 3)
+			matrix.GetLength(1) < 2 || matrix.GetLength(1) > 3 ||
+			matrix.GetLength(0) != matrix.GetLength(1))
 			return -1;
 
 		double[,] sarMatrix = sarrusMatrix(matrix);
