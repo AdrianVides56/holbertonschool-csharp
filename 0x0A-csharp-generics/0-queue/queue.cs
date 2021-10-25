18af258d3b598c8d0b494e0af0724c5a05ae6875
AdrianVides56 <adrianvj18@gmail.com>
2021-10-25 18:07:10 -0500

    Changed code structure

--- 0x0A-csharp-generics/0-queue/queue.cs
+++ 0x0A-csharp-generics/0-queue/queue.cs
@@ -1,9 +1,12 @@
 using System;
 
 
-/// <summary>Initialize class.</summary>
+///<summary>Initialize class.</summary>
 class Queue<T>
 {
-	/// <summary>Returns type Queue's type.</summary>
-	public Type CheckType() => typeof(T);
+	///<summary>Returns Queue's type.</summary>
+	public Type CheckType()
+	{
+		return typeof(T);
+	}
 }
